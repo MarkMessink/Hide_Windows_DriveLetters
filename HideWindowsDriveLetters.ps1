223abb07217293f70c8884cc07ca98cadf6a17c9
Mark Messink <65164974+MarkMessink@users.noreply.github.com>
2022-12-07 18:53:01 +0100

    Create HideWindowsDriveLetters.ps1

---
+++ HideWindowsDriveLetters.ps1
@@ -0,0 +1,157 @@
+<#
+.SYNOPSIS
+	Hide drives in Explorer
+	
+	FileName:    HideDrives.ps1
+    Author:      Mark Messink
+    Contact:     
+    Created:     2021-12-21
+    Updated:     2023-02-02
+
+    Version history:
+    1.0.0 - (2021-12-21) Initial Script
+	1.0.1 - (21-12-2022) Script verniewd
+	1.1.0 - 
+
+.DESCRIPTION
+	Dit script verwijderd de zichtbaarheid van Windows schijven.
+
+.PARAMETER
+	<beschrijf de parameters die eventueel aan het script gekoppeld moeten worden>
+
+.INPUTS
+	Drives		ValueData
+	ShowAll		0
+	A			1
+	B			2
+	C			4
+	D			8
+	E			16
+	F			32
+	G			64
+	H			128
+	I			256
+	J			512
+	K			1024
+	L			2048
+	M			4096
+	N			8192
+	O			16384
+	P			32768
+	Q			65536
+	R			131072
+	S			262144
+	T			524288
+	U			1048576
+	V			2097152
+	W			4194304
+	X			8388608
+	Y			16777216
+	Z			33554432
+	Hide All	67108863
+	If you want to hide multiple drives then add the decimal numbers (such as, for Drive D and E use 24).
+
+.OUTPUTS
+	logfiles:
+	PSlog_<naam>	Log gegenereerd door een powershell script
+	INlog_<naam>	Log gegenereerd door Intune (Win32)
+	AIlog_<naam>	Log gegenereerd door de installer van een applicatie bij de installatie van een applicatie
+	ADlog_<naam>	Log gegenereerd door de installer van een applicatie bij de de-installatie van een applicatie
+	Een datum en tijd wordt automatisch toegevoegd
+
+.EXAMPLE
+	./scriptnaam.ps1
+
+.LINK Information
+
+.NOTES
+	WindowsBuild:
+	Het script wordt uitgevoerd tussen de builds LowestWindowsBuild en HighestWindowsBuild
+	LowestWindowsBuild = 0 en HighestWindowsBuild 50000 zijn alle Windows 10/11 versies
+	LowestWindowsBuild = 19000 en HighestWindowsBuild 19999 zijn alle Windows 10 versies
+	LowestWindowsBuild = 22000 en HighestWindowsBuild 22999 zijn alle Windows 11 versies
+	Zie: https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
+
+#>
+
+#################### Variabelen #####################################
+$logpath = "C:\IntuneLogs"
+$NameLogfile = "PSlog_HideDrives.txt"
+$LowestWindowsBuild = 0
+$HighestWindowsBuild = 50000
+
+
+
+#################### Einde Variabelen ###############################
+
+
+#################### Start base script ##############################
+### Niet aanpassen!!!
+
+# Prevent terminating script on error.
+$ErrorActionPreference = 'Continue'
+
+# Create logpath (if not exist)
+If(!(test-path $logpath))
+{
+      New-Item -ItemType Directory -Force -Path $logpath
+}
+
+# Add date + time to Logfile
+$TimeStamp = "{0:yyyyMMdd-HHmm}" -f (get-date)
+$logFile = "$logpath\" + "$TimeStamp" + "_" + "$NameLogfile"
+
+# Start Transcript logging
+Start-Transcript $logFile -Append -Force
+
+# Start script timer
+$scripttimer = [system.diagnostics.stopwatch]::StartNew()
+
+# Controle Windows Build
+$WindowsBuild = [System.Environment]::OSVersion.Version.Build
+Write-Output "------------------------------------"
+Write-Output "Windows Build: $WindowsBuild"
+Write-Output "------------------------------------"
+If ($WindowsBuild -ge $LowestWindowsBuild -And $WindowsBuild -le $HighestWindowsBuild)
+{
+#################### Start base script ################################
+
+#################### Start uitvoeren script code ####################
+Write-Output "-------------------------------------------------------------------------------------"
+Write-Output "### Start uitvoeren script code ###"
+Write-Output "-------------------------------------------------------------------------------------"
+
+
+	Write-Output "-------------------------------------------------------------------"
+    Write-Output "Hide Drives"
+	#4 = Drive C:
+	New-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDrives" -Value "4" -PropertyType DWORD -Force	
+    
+    Write-Output "-------------------------------------------------------------------"
+
+Write-Output "-------------------------------------------------------------------------------------"
+Write-Output "### Einde uitvoeren script code ###"
+Write-Output "-------------------------------------------------------------------------------------"
+#################### Einde uitvoeren script code ####################
+
+#################### End base script #######################
+
+# Controle Windows Build
+}Else {
+Write-Output "-------------------------------------------------------------------------------------"
+Write-Output "### Windows Build versie voldoet niet, de script code is niet uitgevoerd. ###"
+Write-Output "-------------------------------------------------------------------------------------"
+}
+
+#Stop and display script timer
+$scripttimer.Stop()
+Write-Output "------------------------------------"
+Write-Output "Script elapsed time in seconds:"
+$scripttimer.elapsed.totalseconds
+Write-Output "------------------------------------"
+
+#Stop Logging
+Stop-Transcript
+#################### End base script ################################
+
+#################### Einde Script ###################################
