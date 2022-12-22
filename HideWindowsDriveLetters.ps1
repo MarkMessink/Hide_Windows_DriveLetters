84fa586fa0d0fcb0b946a9381f666b9dd0f9549d
Mark Messink <65164974+MarkMessink@users.noreply.github.com>
2022-12-22 10:51:57 +0100

    Update HideWindowsDriveLetters.ps1

--- HideWindowsDriveLetters.ps1
+++ HideWindowsDriveLetters.ps1
@@ -6,11 +6,11 @@
     Author:      Mark Messink
     Contact:     
     Created:     2021-12-21
-    Updated:     2023-02-02
+    Updated:     2022-12-20
 
     Version history:
     1.0.0 - (2021-12-21) Initial Script
-	1.0.1 - (21-12-2022) Script verniewd
+	1.0.1 - (2022-12-20) Script verniewd
 	1.1.0 - 
 
 .DESCRIPTION
@@ -98,7 +98,7 @@ If(!(test-path $logpath))
 }
 
 # Add date + time to Logfile
-$TimeStamp = "{0:yyyyMMdd-HHmm}" -f (get-date)
+$TimeStamp = "{0:yyyyMMdd}" -f (get-date)
 $logFile = "$logpath\" + "$TimeStamp" + "_" + "$NameLogfile"
 
 # Start Transcript logging
@@ -117,9 +117,9 @@ If ($WindowsBuild -ge $LowestWindowsBuild -And $WindowsBuild -le $HighestWindows
 #################### Start base script ################################
 
 #################### Start uitvoeren script code ####################
-Write-Output "-------------------------------------------------------------------------------------"
-Write-Output "### Start uitvoeren script code ###"
-Write-Output "-------------------------------------------------------------------------------------"
+Write-Output "#####################################################################################"
+Write-Output "### Start uitvoeren script code                                                   ###"
+Write-Output "#####################################################################################"
 
 
 	Write-Output "-------------------------------------------------------------------"
@@ -129,9 +129,9 @@ Write-Output "------------------------------------------------------------------
     
     Write-Output "-------------------------------------------------------------------"
 
-Write-Output "-------------------------------------------------------------------------------------"
-Write-Output "### Einde uitvoeren script code ###"
-Write-Output "-------------------------------------------------------------------------------------"
+Write-Output "#####################################################################################"
+Write-Output "### Einde uitvoeren script code                                                   ###"
+Write-Output "#####################################################################################"
 #################### Einde uitvoeren script code ####################
 
 #################### End base script #######################
